9834ea6ae52935d7e8700e1c3f674d7487f2d571
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-08-14 11:56:43 +0530

    author-entity-created

--- src/main/java/com/lokalib/entity/Author.java
+++ src/main/java/com/lokalib/entity/Author.java
@@ -1,5 +1,46 @@
 package com.lokalib.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="author")
 public class Author {
+	
+	@Id
+	@GeneratedValue()
+	private int id;
+	
+	public Author() {}
+
+	public Author(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	private String firstName;
+	private String lastName;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 }
