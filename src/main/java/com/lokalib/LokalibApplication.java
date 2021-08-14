bced2f227696316c7d18eff2d122450a1a7d14e1
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-08-14 11:35:38 +0530

    lokalib-spring-starter-project-created

---
+++ src/main/java/com/lokalib/LokalibApplication.java
@@ -0,0 +1,13 @@
+package com.lokalib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokalibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokalibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lokalib/LokalibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokalib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokalibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
